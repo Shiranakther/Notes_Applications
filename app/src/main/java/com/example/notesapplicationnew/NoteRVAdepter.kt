dbc37193c35152a867b27311c3fb628ff53635f2
Shiran <127624730+Shiranakther@users.noreply.github.com>
2024-05-12 15:26:10 +0530

    Create,Update Delete functions implementation

--- app/src/main/java/com/example/notesapplicationnew/NoteRVAdepter.kt
+++ app/src/main/java/com/example/notesapplicationnew/NoteRVAdepter.kt
@@ -1,5 +1,54 @@
 package com.example.notesapplicationnew
 
-class NoteRVAdepter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
 
+class NoteRVAdepter(
+    val context:Context,
+    val noteClickInterface: NoteClickInterface,
+    val noteClickDeleteInterface: NoteClickDeleteInterface
+    ) :RecyclerView.Adapter<NoteRVAdepter.ViewHolder>(){
+
+        private val allNotes = ArrayList<Note>()
+        inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+            val noteTV = itemView.findViewById<TextView>(R.id.idTVNoteTitle)
+            val timeTV = itemView.findViewById<TextView>(R.id.idTVTimeStamp)
+            val deleteTV = itemView.findViewById<ImageView>(R.id.idTVDelete)
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note_rv_item,parent,false);
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.noteTV.setText(allNotes.get(position).noteTitle)
+        holder.timeTV.setText("Last Updated : "+allNotes.get(position).timeStamp)
+
+        holder.deleteTV.setOnClickListener{
+            noteClickDeleteInterface.onDeleteIconClick(allNotes.get(position))
+        }
+
+        holder.itemView.setOnClickListener{
+            noteClickInterface.onNoteClick(allNotes.get(position))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+interface NoteClickDeleteInterface{
+    fun onDeleteIconClick(note:Note)
+}
+
+interface NoteClickInterface{
+    fun onNoteClick(note: Note)
 }
